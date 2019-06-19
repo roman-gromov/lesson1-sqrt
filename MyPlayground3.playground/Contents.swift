4d1b64b6d4913d27b87dc7e3fea4e1e3e094ccc8
Roman Gromov <ios@gromov.site>
2019-06-20 00:34:04 +0500

    без ввода пользователем

--- MyPlayground3.playground/Contents.swift
+++ MyPlayground3.playground/Contents.swift
@@ -4,13 +4,29 @@ var sm: Double = 0
 var sy: Double = 0
 var sa: Double = 0
 
+
 let sum: Double = 1000000
 let perc: Double = 12
 
+sm = sum
+sy = sum
 
-sm = (sum+sum*perc/100)/12
-sy = sum+sum*perc/100
-sa = 5*(sum+sum*perc/100)
+func addm () {sm = sm+sm*(perc/12)/100}
+
+for _ in 1...60 {
+    addm ()
+}
 
 
-print("с ежемесячной капитализацией \(sm), с ежегодной капитализацией \(sy), без капитализации\(sa)")
+func addy () {sy = sy+sy*(perc)/100}
+
+for _ in 1...5 {
+    addy ()
+}
+
+sm = (sm*100).rounded()/100
+sy = (sy*100).rounded()/100
+sa = round(sum+5*sum*perc/100)
+
+
+print("с ежемесячной капитализацией \(sm), с ежегодной капитализацией \(sy), без капитализации \(sa)")
