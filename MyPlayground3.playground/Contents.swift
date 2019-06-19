7d289e9d3698fc924107e1a4a01809532a64f4f1
Roman Gromov <ios@gromov.site>
2019-06-19 23:36:19 +0500

    сохранил

--- MyPlayground3.playground/Contents.swift
+++ MyPlayground3.playground/Contents.swift
@@ -1,3 +1,16 @@
 import UIKit
 
-var str = "Hello, playground"
+var sm: Double = 0
+var sy: Double = 0
+var sa: Double = 0
+
+let sum: Double = 1000000
+let perc: Double = 12
+
+
+sm = (sum+sum*perc/100)/12
+sy = sum+sum*perc/100
+sa = 5*(sum+sum*perc/100)
+
+
+print("с ежемесячной капитализацией \(sm), с ежегодной капитализацией \(sy), без капитализации\(sa)")
